130dccd01b4d2af9735ca8120e186566c905a81e
Joe Meyer <j23meyerj@gmail.com>
2021-01-17 14:36:26 -0600

    corrected error with image array.

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,8 +1,8 @@
 const galleryItems = [
     { id: 1, path: 'images/DSC_0893.jpg', description: 'Ready to go out and get some candy.', likes: 0 },
-    { id: 2, path: 'images/IMG_1757.jpg', description: 'Family picture at nephew wedding.', likes: 0 }
-    { id: 3, path: 'images/DSC_0507.jpg', description: 'Enjoying vacation time.', likes: 0 }
-    { id: 4, path: 'images/DSC_0158.jpg', description: 'All dressed up for the wedding.', likes: 0 }
+    { id: 2, path: 'images/IMG_1757.jpg', description: 'Family picture at nephew wedding.', likes: 0 },
+    { id: 3, path: 'images/DSC_0507.jpg', description: 'Enjoying vacation time.', likes: 0 },
+    { id: 4, path: 'images/DSC_0158.jpg', description: 'All dressed up for the wedding.', likes: 0 },
     { id: 5, path: 'images/DSC_0572.jpg', description: 'Having fun outside.', likes: 0 }
 ];
 
